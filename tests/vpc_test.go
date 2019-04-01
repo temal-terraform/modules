2c9f814022c8b89b2309a90c7f44a81f962ca5a2
Malte Krupa <github@nafn.de>
2019-04-01 13:58:57 +0200

    Add test stages via the test_structure module and fix test by actually getting information from AWS via the SDK

--- tests/vpc_test.go
+++ tests/vpc_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	terragrunt_aws "github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,6 +18,8 @@ var environment = "bar"
 var second_octet = "100"
 
 func TestVpcModule(t *testing.T) {
+	t.Parallel()
+
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../vpc",
 		Vars: map[string]interface{}{
@@ -23,20 +28,48 @@ func TestVpcModule(t *testing.T) {
 			"second_octet": second_octet,
 		},
 	}
-	defer terraform.Destroy(t, terraformOptions)
 
-	terraform.InitAndApply(t, terraformOptions)
-	validateVpc(t, terraformOptions)
+	// At the end of the test, undeploy terraform module code
+	defer test_structure.RunTestStage(t, "cleanup_terraform", func() {
+		terraform.Destroy(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "deploy_terraform", func() {
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "test_vpc", func() {
+		validateVpc(t, terraformOptions)
+	})
 }
 
 func validateVpc(t *testing.T, o *terraform.Options) {
 	// Check CIDR block
-	vpc_cidr_block := terraform.Output(t, o, "vpc_cidr_block")
-	assert.Equal(t, "10."+second_octet+".0.0/16", vpc_cidr_block)
+	vpcCidrBlock := terraform.Output(t, o, "vpc_cidr_block")
+	assert.Equal(t, "10."+second_octet+".0.0/16", vpcCidrBlock)
 
 	// Check name
-	vpc_id := terraform.Output(t, o, "vpc_id")
-	vpc := ec2.Vpc{VpcId: aws.String(vpc_id)}
-	vpc_name := terragrunt_aws.FindVpcName(&vpc)
-	assert.Equal(t, environment+"-"+tenant, vpc_name)
+	// Get vpc id from terraform output
+	vpcId := terraform.Output(t, o, "vpc_id")
+
+	// Create session for aws sdk
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	// Get newly created VPC from AWS
+	svc := ec2.New(sess)
+	input := &ec2.DescribeVpcsInput{
+		VpcIds: []*string{
+			aws.String(vpcId),
+		},
+	}
+	result, err := svc.DescribeVpcs(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	vpc := result.Vpcs[0]
+
+	// Get VPC name from previously created VPC object
+	vpcName := terragrunt_aws.FindVpcName(vpc)
+	assert.Equal(t, environment+"-"+tenant, vpcName)
 }
