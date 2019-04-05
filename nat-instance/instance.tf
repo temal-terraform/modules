d6621c4238f2ad142b9093e4f379fcca30a30964
Malte Krupa <github@nafn.de>
2019-04-05 15:50:37 +0200

    Fix security group for nat instances

--- nat-instance/instance.tf
+++ nat-instance/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "nat_1a" {
   ami                         = "${data.aws_ami.nat_ami.id}"
   availability_zone           = "eu-central-1a"
   instance_type               = "t3.micro"
-  vpc_security_group_ids      = ["${aws_security_group.nat_security_group.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.nat.id}"]
   subnet_id                   = "${data.aws_subnet.public_1a.id}"
   associate_public_ip_address = true
   source_dest_check           = false
@@ -20,7 +20,7 @@ resource "aws_instance" "nat_1b" {
   ami                         = "${data.aws_ami.nat_ami.id}"
   availability_zone           = "eu-central-1b"
   instance_type               = "t3.micro"
-  vpc_security_group_ids      = ["${aws_security_group.nat_security_group.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.nat.id}"]
   subnet_id                   = "${data.aws_subnet.public_1b.id}"
   associate_public_ip_address = true
   source_dest_check           = false
@@ -38,7 +38,7 @@ resource "aws_instance" "nat_1c" {
   ami                         = "${data.aws_ami.nat_ami.id}"
   availability_zone           = "eu-central-1c"
   instance_type               = "t3.micro"
-  vpc_security_group_ids      = ["${aws_security_group.nat_security_group.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.nat.id}"]
   subnet_id                   = "${data.aws_subnet.public_1c.id}"
   associate_public_ip_address = true
   source_dest_check           = false
